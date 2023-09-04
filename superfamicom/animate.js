aaa5bda0aea302acb2b9bd2533cbedd660f01259
SEIMIN-BJJ <interkq12@gmail.com>
2023-09-04 23:32:27 +0900

    .

--- superfamicom/animate.js
+++ superfamicom/animate.js
@@ -1,145 +1,132 @@
-    const next = document.querySelector('.next');
-    const move = next.children;
-    let move1 = [{
-        transform: 'translateY(-40px)'
-      },
-      {
-        transform: 'translateY(10px)'
-      },
-      {
-        transform: 'translateY(-40px)'
-      }
-    ];
+const next = document.querySelector(".next");
+const move = next.children;
+let move1 = [
+  {
+    transform: "translateY(-40px)",
+  },
+  {
+    transform: "translateY(10px)",
+  },
+  {
+    transform: "translateY(-40px)",
+  },
+];
 
-    let move2 = {
-      duration: 1500,
-      interations: Infinity
-    };
+let move2 = {
+  duration: 1500,
+  interations: Infinity,
+};
 
-    setInterval(() => {
+setInterval(() => {
+  next.animate(move1, move2);
+}, 1500);
 
-      next.animate(move1, move2);
-    }, 1500);
+const next1 = document.querySelector(".next1");
+const moving = next1.children;
+let move3 = [
+  {
+    transform: "translateY(-40px)",
+  },
+  {
+    transform: "translateY(10px)",
+  },
+  {
+    transform: "translateY(-40px)",
+  },
+];
 
-    const next1 = document.querySelector('.next1');
-    const moving = next1.children;
-    let move3 = [{
-        transform: 'translateY(-40px)'
-      },
-      {
-        transform: 'translateY(10px)'
-      },
-      {
-        transform: 'translateY(-40px)'
-      }
-    ];
+let move4 = {
+  duration: 1500,
+  interations: Infinity,
+};
 
-    let move4 = {
-      duration: 1500,
-      interations: Infinity
-    };
+setInterval(() => {
+  next1.animate(move3, move4);
+}, 1500);
 
-    setInterval(() => {
+const next2 = document.querySelector(".next2");
+const moving1 = next2.children;
+let move5 = [
+  {
+    transform: "translateX(-13px)",
+  },
+  {
+    transform: "translateX(10px)",
+  },
+  {
+    transform: "translateX(-13px)",
+  },
+];
 
-      next1.animate(move3, move4);
-    }, 1500);
+let move6 = {
+  duration: 1500,
+  interations: Infinity,
+};
 
-    const next2 = document.querySelector('.next2');
-    const moving1 = next2.children;
-    let move5 = [{
-        transform: 'translateX(-13px)'
-      },
-      {
-        transform: 'translateX(10px)'
-      },
-      {
-        transform: 'translateX(-13px)'
-      }
-    ];
+setInterval(() => {
+  next2.animate(move5, move6);
+}, 1500);
 
-    let move6 = {
-      duration: 1500,
-      interations: Infinity
-    };
+//-----------------------------------------------------------
+const root = document.getElementById("root");
+const power = document.querySelector(".power1");
+const light = document.querySelector(".light");
+const famicom = document.querySelector(".famicom");
 
-    setInterval(() => {
+let data = true;
+power.addEventListener("click", () => {
+  if (data === true) {
+    data = false;
+    power.style.transform = "translateY(-25px)";
+    power.style.transition = "0.3s";
+    light.style.backgroundColor = "#D25050";
+    famicom.style.transition = "1s";
+    famicom.style.opacity = "1";
 
-      next2.animate(move5, move6);
-    }, 1500);
+    console.log(power);
+  } else {
+    data = true;
+    power.style.transform = "translateY(0px)";
+    power.style.transition = "0.3s";
+    light.style.backgroundColor = "#585858";
+    famicom.style.transition = "1s";
+    famicom.style.opacity = "0";
 
+    console.log(light);
+  }
+});
 
-    //-----------------------------------------------------------
-    const root = document.getElementById('root');
-    const power = document.querySelector('.power1');
-    const light = document.querySelector('.light');
-    const famicom = document.querySelector('.famicom');
+const abtn = document.querySelector(".cir-8");
+const swi = document.querySelector(".swi");
 
-    let data = true;
-    power.addEventListener('click', () => {
+let data1 = true;
+abtn.addEventListener("click", () => {
+  if (data1 === true) {
+    data1 = false;
+    swi.style.opacity = "0";
+    swi.style.transition = "1s";
 
-      if (data === true) {
-        data = false;
-        power.style.transform = "translateY(-25px)";
-        power.style.transition = "0.3s";
-        light.style.backgroundColor = "#D25050";
-        famicom.style.transition = "1s";
-        famicom.style.opacity = "1";
+    console.log(power);
+  } else {
+    data1 = true;
+    swi.style.opacity = "1";
+    swi.style.transition = "1s";
 
-        console.log(power);
+    console.log(light);
+  }
+});
 
-      } else {
-        data = true;
-        power.style.transform = "translateY(0px)";
-        power.style.transition = "0.3s";
-        light.style.backgroundColor = "#585858";
-        famicom.style.transition = "1s";
-        famicom.style.opacity = "0";
+const up = document.querySelector(".fa-solid");
 
-        console.log(light);
-
-      }
-    });
-
-    const abtn = document.querySelector('.cir-8');
-    const swi = document.querySelector('.swi');
-
-    let data1 = true;
-    abtn.addEventListener('click', () => {
-
-      if (data1 === true) {
-        data1 = false;
-        swi.style.opacity = "0";
-        swi.style.transition = "1s";
-
-        console.log(power);
-
-      } else {
-        data1 = true;
-        swi.style.opacity = "1";
-        swi.style.transition = "1s";
-
-        console.log(light);
-
-      }
-    });
-
-    const up = document.querySelector('.fa-solid');
-
-up.addEventListener('click', (event) => {
-
+up.addEventListener("click", (event) => {
   window.scrollTo({
-
     top: 0,
     behavior: "smooth",
   });
-
 });
 
-
-const fort = document.querySelector('.body4-1');
-fort.addEventListener('click', function(){
-let page = 'https://seimin123.github.io/VER-1/';
-location.href = page;
+const fort = document.querySelector(".body4-1");
+fort.addEventListener("click", function () {
+  let page = "https://seimin-bjj.github.io/PortFolio-R/";
+  location.href = page;
 });
-
-
